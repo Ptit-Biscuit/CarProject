3d8cc45264b63e79c4a1b671c8b48f5aaeb5c4c2
Ptit-Biscuit <vincent.brebion@epsi.fr>
2020-10-05 19:06:23 +0200

    fix

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from subprocess import call
 from car_controller import CarController
 
+
+def on_disconnect():
+    call("sudo reboot", shell=True)
+
+
 controller = CarController(interface="/dev/input/js0")
-controller.listen(on_disconnect=(call("sudo reboot", shell=True)))
+controller.listen(on_disconnect=on_disconnect)
